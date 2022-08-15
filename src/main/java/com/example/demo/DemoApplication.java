4551617a39c34dd340fac6925fa9ad2a3f4c1baf
Clement Boret <cboret@squareup.com>
2022-08-15 13:38:15 +0200

    cd /Users/cboret/code/2delete/debug ; /usr/bin/env /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin/java -agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=localhost:50724 -XX:+ShowCodeDetailsInExceptionMessages @/var/folders/yj/6dmmh9zj4gl1zyg1cvqsd0sm0000gn/T/cp_doxwuevd4t289xjdl0iabam3h.argfile com.example.demo.DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.tidal.spring.filters.annotations.EnableLogRequestResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,7 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableLogRequestResponse
 @Slf4j
 public class DemoApplication {
 
@@ -45,18 +45,18 @@ public class DemoApplication {
 		}
 	}
 
-	@Bean
-	public CommonsRequestLoggingFilter logFilter() {
-		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+	// @Bean
+	// public CommonsRequestLoggingFilter logFilter() {
+	// 	CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
 
-		filter.setIncludeQueryString(true);
-		filter.setIncludePayload(true);
-		filter.setMaxPayloadLength(10000);
-		filter.setIncludeHeaders(true);
-		filter.setIncludeClientInfo(true);
+	// 	filter.setIncludeQueryString(true);
+	// 	filter.setIncludePayload(true);
+	// 	filter.setMaxPayloadLength(10000);
+	// 	filter.setIncludeHeaders(true);
+	// 	filter.setIncludeClientInfo(true);
 
-		return filter;
-	}
+	// 	return filter;
+	// }
 
 	@Data
 	@Builder
